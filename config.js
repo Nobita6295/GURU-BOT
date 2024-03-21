b7f0ef77eb684a80b3c603e212ba0bab6567727b
Nobita6295 <164227398+Nobita6295@users.noreply.github.com>
2024-03-21 22:48:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 global.owner = [
   ['9474377020', 'nobita bot', true],
-  ['6281255369012', 'Ash', true], 
+  ['9474377020', 'nobita bot', true], 
   [''] 
 ] //Number of owners
 
